ab2464b884e0d3a720f1a3407f703663a8bc445b
Jeff <doylejeff98@gmail.com>
2023-03-05 18:58:09 -0500

    profile query slector

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 const X_CLASS ='x'; // Treecko
 const Y_CLASS ='y'; // Mudkip
 const Z_CLASS ='z'; // Torchic
-//
+
+
+// User selected on start screen function 
+
+function Profile(){
+    let selectedProfile = document.querySelectorAll('.start-game .img .id');
+}
