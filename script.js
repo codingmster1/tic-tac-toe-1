593a494e3e7cc135127bc8605c32bcd07291053f
Jeff <doylejeff98@gmail.com>
2023-03-05 19:26:52 -0500

    img slected added

--- script.js
+++ script.js
@@ -10,6 +10,8 @@ function Profile(){
     selectedProfile.forEach(img => {
         img.addEventListener("click", e => {
 
+            let target = e.target.dataset.id;
+            document.querySelector(`[data-id='${target}']`).classList.add("selected");
             console.log(e.target.dataset.id);
         });
         
